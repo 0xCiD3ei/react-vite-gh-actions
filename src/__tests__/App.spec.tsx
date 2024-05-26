01e0e29ec160ed7599cdd27946abdbadc74f3029
CJ <duytienabc123@gmail.comn>
2024-05-26 22:54:13 +0700

    :white_check_mark: add pass test

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -8,3 +8,13 @@ describe('Renders the main page', () => {
     expect(true).toBeTruthy()
   })
 })
+
+describe('something truthy and falsy', () => {
+  test('true to be true', () => {
+    expect(true).toBe(true)
+  })
+
+  test('false to be false', () => {
+    expect(false).toBe(false)
+  })
+})
